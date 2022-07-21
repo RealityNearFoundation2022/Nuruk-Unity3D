8e589460ac3429f540dd7632d6c6fc88be95a6fa
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2022-07-21 15:15:47 -0500

    position tutorial

--- Assets/Assets/Scripts/City/GameManager.cs
+++ Assets/Assets/Scripts/City/GameManager.cs
@@ -5,14 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-   // [SerializeField] GameObject popUpIniciarSesionWallet;
+    [SerializeField] GameObject miniTutorial;
+    
+    void Start()
+    {
+        StartCoroutine(WaitMiniTutorial());
+    }
+    IEnumerator WaitMiniTutorial(){
+        yield return new WaitForSeconds(10f);
+        miniTutorial.SetActive(false);
+    }
+    public void CursorManagerOFF(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void CursorManagerON(){
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void GoToLoginNear()
     {
         Debug.Log("click");
         SceneManager.LoadScene("LoginNear");
     }
-  /*   public void ClosePopUp()
-    {
-        popUpIniciarSesionWallet.SetActive(false);
-    } */
 }
